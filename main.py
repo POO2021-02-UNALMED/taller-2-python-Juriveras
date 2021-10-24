3b7a1854163ce20846fa4689ec0acf3e41c0d4f8
Juan Pablo Rivera Sierra <54080237+Juriveras@users.noreply.github.com>
2021-10-24 17:43:35 -0500

    Created main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        cant = 0
+        for i in asientos:
+            if type(i) == "Asiento":
+                cant += 1
+        return cant
+
+    def verificarIntegridad(self):
+        if(self.motor.registro != self.registro):
+            return "Las piezas no son originales"
+        for i in self.asientos:
+            if(type(i) == "Asiento"):
+                if(self.registro != i.registro):
+                    return "Las piezas no son originales"
+        return "Auto original"
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if(color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+            self.color = color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if(tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
